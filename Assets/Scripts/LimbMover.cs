d109f3be48bcd49de39833e9ab0abf9ee2a9bfc9
Haley <hne3@github.com>
2016-07-26 17:43:43 -0400

    Added spaceship environment + full body movement with space

--- Assets/Scripts/LimbMover.cs
+++ Assets/Scripts/LimbMover.cs
@@ -1,24 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
 public class LimbMover : MonoBehaviour
 {
-
     public string Axis;
     public float ForceScale = 10f;
     public float MaxForce = 1000f;
 
     private Rigidbody rgb;
-
-	// Use this for initialization
-	void Start ()
+    
+	private void Start ()
 	{
 	    rgb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
+	private void FixedUpdate ()
+    {
 	    if (Input.GetButton(Axis))
 	    {
 	        Vector3 mouseDelta = new Vector3 {x = Mathf.Clamp01(Input.GetAxis("Horizontal")), y = Mathf.Clamp01(Input.GetAxis("Vertical")), z = Mathf.Clamp01(Input.mouseScrollDelta.y)};
