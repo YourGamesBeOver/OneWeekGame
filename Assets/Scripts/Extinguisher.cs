b92c6a3ab28b6bcb8b28974c50c58b37184b2812
Steven Miller <t-stmill@microsoft.com>
2016-07-29 13:42:57 -0700

    Fires can now be put out

---
+++ Assets/Scripts/Extinguisher.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Cecil;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Extinguisher : MonoBehaviour
+{
+    private ParticleCollisionEvent[] collisionEvents;
+    private ParticleSystem particles;
+
+    [SerializeField]
+    private float Strength = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        collisionEvents = new ParticleCollisionEvent[16];
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        if (!other.CompareTag("Fire")) return;
+        int safeLength = particles.GetSafeCollisionEventSize();
+        if (collisionEvents.Length < safeLength)
+        {
+            collisionEvents = new ParticleCollisionEvent[safeLength];
+        }
+        int numCollisionEvents = particles.GetCollisionEvents(other, collisionEvents);
+
+        other.GetComponent<Fire>().Extinguish(numCollisionEvents * Strength);
+    }
+}
---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private float Strength;
+    private Vector3 startingScale;
+
+    [SerializeField]
+    private float MaxStrength = 100f;
+
+    [SerializeField]
+    private AnimationCurve SizeCurve;
+
+    [SerializeField]
+    private ParticleSystem[] particles;
+
+    // Use this for initialization
+    private void Start ()
+    {
+        Strength = MaxStrength;
+        startingScale = transform.localScale;
+    }
+	
+	// Update is called once per frame
+	private void Update () {
+	    DebugHUD.setValue("Strength", string.Format("{0}/{1} ({2}%)", Strength, MaxStrength, Strength/MaxStrength));
+	}
+
+    public void Extinguish(float amount)
+    {
+        Debug.Log(string.Format("Extinguished {0}", amount));
+        Strength -= amount;
+        if (Strength <= 0)
+        {
+            Destroy(gameObject, 10);
+            foreach (var p in particles)
+            {
+                p.Stop();
+            }
+        }
+        else
+        {
+            transform.localScale = startingScale*SizeCurve.Evaluate(Strength/MaxStrength);
+        }
+    }
+}
